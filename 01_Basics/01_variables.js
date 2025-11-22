1385f7d5c6e84c203f1a8acea093570594f8c4a5
Manorama pradhan <102949712+manorama6610@users.noreply.github.com>
2025-11-22 08:42:53 +0000

    Add JS basics files

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId= 144335
+let accountEmail= "pradhanmanorama2@gmail.com"
+var accountPassword="123445"
+accountCity="Odisha"
+accountEmail= "pradhanm@gmail.com"
+accountPassword="123f45"
+accountCity="kolkata"
+console.table([accountId,accountEmail,accountPassword,accountCity])
---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) // we are using Nodejs , not browser
+
+console.log(3+3) // code readability should be high
+
+console.log("manorama")
+
+let name="Manorama" //String Datatype
+let age=22
+let isLoggedIn= false
+
+
+//primitive datatypes
+
+// number => 2 to power 53
+// bigint
+// string =>""
+// boolean=>true/false
+// null => stand alone value
+//undefined => not assigned
+// symbol => unique
+
+// object
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
