ee1450c5fcf13b629dd7262f1ef7b1209585adfc
Manorama pradhan <102949712+manorama6610@users.noreply.github.com>
2025-11-24 03:07:52 +0000

    Conversion of Datatypes

--- 01_Basics/03_conversionoperation.js
+++ 01_Basics/03_conversionoperation.js
@@ -23,6 +23,18 @@ let stringNumber =String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
 
+// **************** Operations****************
+
+let value=3
+let negValue= -value
+console.log(negValue);
+
+let gameCounter=100
+++gameCounter;
+console.log(gameCounter);
+
+
+
 
 
 
