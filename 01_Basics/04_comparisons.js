fd5abc02e3d66347fff1272a0f383d3a05415af9
Manorama pradhan <102949712+manorama6610@users.noreply.github.com>
2025-11-24 03:41:48 +0000

    Comparisons

---
+++ 01_Basics/04_comparisons.js
@@ -0,0 +1,11 @@
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);  //true
+// Avoide the above comparisons.
+// === #checks strictly by comparing the datatypes
+console.log("2"===2);
+
+
+
+
+
