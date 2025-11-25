c34fb117cb87514f854071bac636c4643f3b536d
Manorama pradhan <102949712+manorama6610@users.noreply.github.com>
2025-11-25 10:22:28 +0000

    Datatypes

--- 01_Basics/datatype-summary.js
+++ 01_Basics/datatype-summary.js
@@ -35,3 +35,25 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// stack(primitive type)  Heap(Non-primitive type)
+
+let myYoutubename="manorama"
+
+let anotherName = myYoutubename
+anotherName = "M_Manorama"
+console.log("myYoutubename");
+console.log("anotherName");
+
+let user1 = {
+    email:"User@google.com",
+    upi: "user@ybl"
+}
+let user2 = user1
+
+user2.email="manorama@google.com"
+console.log(user1.email);
+console.log(user2.email);
+
